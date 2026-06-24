33d04c07c8620765aba5364cd06a664f2bfe7705
idontwanttobeanurseanymore <martaariasolmo@hotmail.com>
2026-06-24 14:28:53 +0200

    conexion ok, frontend fetch ok. Now it throws a CSP error after changing GET to show a list.

---
+++ BACKEND/src/index.js
@@ -0,0 +1,96 @@
+import express from "express";
+import path from "node:path";
+import cors from "cors";
+import mysql from "mysql2/promise"
+import 'dotenv/config';
+
+// Configurar el servidor
+const server = express();
+
+// Configuración para que funcione como API RESTful (json)
+server.use(cors()); // API pública
+server.use(express.json({ limit: "25Mb" }));
+
+// Configuramos Express para que use EJS:
+//server.set("view engine", "ejs");
+
+// Arrancamos
+const port = process.env.PORT;
+server.listen(port, () => {
+  console.log(`El servidor se ha iniciado en <http://localhost:${port}/>`);
+});
+
+// Configuración de MySQL
+
+const getConexion = async () => {
+  const datosConexion = {
+    host: process.env.MYSQL_HOST || "localhost",
+    port: process.env.MYSQL_PORT || 3306,
+    user: process.env.MYSQL_USER || "root",
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_SCHEMA,
+  };
+
+  const conexion = await mysql.createConnection(datosConexion); // Crear la cajita de la conexión en el Workbench
+  await conexion.connect(); // Hacer click en la cajita de la conex del Workbench
+
+  return conexion;
+};
+
+//ENDPOINTS (method+path)
+
+//doctorwho
+server.get("/doctorwho", async (req, res) => {
+  const conexion = await getConexion();
+  const queryListDoctors= `
+  SELECT doctors.nombre
+  FROM doctorwho.doctors
+  `
+  const [resultado] = await conexion.query(queryListDoctors);
+  await conexion.end();
+  res.render("doctores-list", {
+    doctores: resultado
+  })
+    res.json({ message: resultado });
+});
+
+server.get("/doctorwho/:id", (req, res) => {
+    res.json({ message: `Personaje con id: ${id}` });
+});
+//Otros GET: 
+  // /doctorwho/search 
+  // /doctorwho/profile
+
+server.post("/doctorwho", (req, res) => {
+  const nuevoPersonaje = req.body;
+  res.json({
+    message: 'Personaje creado',
+    data: nuevoPersonaje
+  });
+});
+//PUT companion, enemigos, planetas
+
+server.put('/doctorwho/doctors/:id', (req, res) => {
+  const id = req.params.id;
+  const datos = req.body;
+
+  res.json({
+    message: `Personaje ${id} actualizado`,
+    data: datos
+  });
+});
+
+server.delete('/doctorwho/:id', (req, res) => {
+  const id = req.params.id;
+
+  res.json({
+    message: `Personaje ${id} eliminado`
+  });
+});
+
+//Si la ruta no está bien escrita
+server.get(/.*/, (req, res) => {
+  //GET http://localhost:4000/*
+  res.status(404).send("Página no encontrada.");
+});
+
